a9b2f3c6e88d27ee0acc2b9643f0e819b7cad84d
Emanuel <micorreo@gmail.com>
2026-06-12 16:46:12 -0300

    se adapta el settings.py para la carpeta media

--- tecnomarema/settings.py
+++ tecnomarema/settings.py
@@ -933,8 +933,8 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_URL = 'https://res.cloudinary.com/.../'  # se genera automáticamente
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# MEDIA_URL = 'https://res.cloudinary.com/'  # se genera automáticamente
 
 
 
